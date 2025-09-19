6bc77f6f20f8eed5e0788d9eca1b9a826557c128
Arvie1290 <arviefaezaalauya3@gmail.com>
2025-09-19 21:02:37 +0700

    Update MyScript.lua

--- MyScript.lua
+++ MyScript.lua
@@ -110,7 +110,7 @@ local buttons = {
     {name = "Steal A Brainrot (V0.8)", url = "https://raw.githubusercontent.com/Arvie1290/Natural_Disaster/refs/heads/Steal-A-Brainrot-Modded-Only/SAB_Modded.lua"},
     {name = "Natural Disaster (V1.0)", url = "https://raw.githubusercontent.com/Arvie1290/Natural_Disaster/Natural-Disaster/Natural_Disaster.lua"},
     {name = "Pull A Friend! (V0.9)", url = "https://raw.githubusercontent.com/Arvie1290/Natural_Disaster/refs/heads/Pull-A-Friend!/World1.lua"},
-    {name = "TP/Tween Gui (V1.0)", url = "https://raw.githubusercontent.com/Arvie1290/Natural_Disaster/refs/heads/TP_Tween_Gui/TP_Tween_Gui.lua"
+    {name = "TP/Tween Gui (V1.0)", url = "https://raw.githubusercontent.com/Arvie1290/Natural_Disaster/refs/heads/TP_Tween_Gui/TP_Tween_Gui.lua"},
     -- tambah lagi disini
 }
 
